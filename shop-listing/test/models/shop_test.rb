9fe17e345c633331ade8a5fb90d0ed910e828261
taha <mohamedtaha.boutayeb@gmail.com>
2019-12-26 15:22:59 -0100

    chore: add shop model tests

--- shop-listing/test/models/shop_test.rb
+++ shop-listing/test/models/shop_test.rb
@@ -1,7 +1,25 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ShopTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @shop = Shop.new(name: 'Shop1', distance: 100)
+  end
+  test 'valid shop' do
+    assert @shop.valid?
+  end
+  test 'invalid shop without name' do
+    @shop.name = nil
+    refute @shop.valid?, 'saved shop without a name'
+    assert_not_nil @shop.errors[:name], 'no validation error for name present'
+  end
+  test 'invalid shop without distance' do
+    @shop.distance = nil
+    refute @shop.valid?, 'saved shop without a distance'
+    assert_not_nil @shop.errors[:distance]
+  end
 end
